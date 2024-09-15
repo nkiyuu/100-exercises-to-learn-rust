2c88ecd49f4ac0f0677359bfca04797c8fa9b3c3
Yuki Nozaki <yuki_nozaki@voyagegroup.com>
2024-09-15 18:53:47 +0900

    02_06完了

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,14 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut ans = 1;
+    let mut i = n;
+    while i > 0 {
+        ans *= i;
+        i -= 1;
+    }
+
+    ans
 }
 
 #[cfg(test)]
