a36b4047a3760be9265745223437b60ffc716131
Yuki Nozaki <yuki_nozaki@voyagegroup.com>
2024-09-23 00:21:40 +0900

    03完了

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,83 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        if !Self::is_valid_product_name(&product_name) {
+            panic!("Product name is invalid");
+        }
+
+        if !Self::is_valid_quantity(quantity) {
+            panic!("Quantity is invalid");
+        }
+
+        if !Self::is_valid_unit_price(unit_price) {
+            panic!("Unit price is invalid");
+        }
+
+        Self {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn product_name(&self) -> &str {
+        &self.product_name
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        if !Self::is_valid_product_name(&product_name) {
+            panic!("Product name is invalid");
+        }
+
+        self.product_name = product_name;
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        if !Self::is_valid_quantity(quantity) {
+            panic!("Quantity is invalid");
+        }
+
+        self.quantity = quantity;
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        if !Self::is_valid_unit_price(unit_price) {
+            panic!("Unit price is invalid");
+        }
+
+        self.unit_price = unit_price;
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+
+    fn is_valid_product_name(product_name: &str) -> bool {
+        !product_name.is_empty() && product_name.len() <= 300
+    }
+
+    fn is_valid_quantity(quantity: u32) -> bool {
+        quantity > 0
+    }
+
+    fn is_valid_unit_price(unit_price: u32) -> bool {
+        unit_price > 0
+    }
+}
